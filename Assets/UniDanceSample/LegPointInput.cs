122915fa28e46d9c2a4cc77f34e93e7a22a13c8a
EsProgram <radwimps.september@gmail.com>
2018-01-26 00:01:06 +0900

    add dance scene sample

---
+++ Assets/UniDanceSample/LegPointInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Es.WaveformProvider;
+
+public class LegPointInput : MonoBehaviour
+{
+	public Texture2D waveform;
+	public int paintTiming = 3;
+
+	[Range(0, 1)]
+	public float scale = 0.01f;
+
+	[Range(0, 1)]
+	public float strength = 0.01f;
+
+	private int count;
+
+	private void OnCollisionStay(Collision collision)
+	{
+		count++;
+		if (count >= paintTiming)
+		{
+			count = 0;
+			foreach (var p in collision.contacts)
+			{
+				var waveConductor = p.otherCollider.GetComponent<WaveConductor>();
+				if (waveConductor != null)
+					waveConductor.Input(waveform, p.point, scale, strength);
+			}
+		}
+	}
+}
---
+++ Assets/UniDanceSample/PlayAudioDlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudioDlay : MonoBehaviour
+{
+	// Use this for initialization
+	public float delay;
+
+	private void Start()
+	{
+		GetComponent<AudioSource>().Play((ulong)(44100 * delay));
+	}
+}

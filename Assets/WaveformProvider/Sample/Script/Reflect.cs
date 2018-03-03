a96bb7cf50b25049e22e0472669a74c6ac27af6d
EsProgram <radwimps.september@gmail.com>
2018-03-04 04:43:56 +0900

    clip to oblique projection matrix

--- Assets/WaveformProvider/Sample/Script/Reflect.cs
+++ Assets/WaveformProvider/Sample/Script/Reflect.cs
@@ -11,6 +11,9 @@ namespace Es.WaveformProvider.Sample
 		[SerializeField]
 		private Camera reflectCamera;
 
+		[SerializeField]
+		private float nearClipOffset;
+
 		private new Renderer renderer;
 		private Material sharedMaterial;
 
@@ -20,8 +23,18 @@ namespace Es.WaveformProvider.Sample
 		{
 			renderer = GetComponent<Renderer>();
 			sharedMaterial = renderer.sharedMaterial;
+			reflectCamera.projectionMatrix = reflectCamera.CalculateObliqueMatrix(CalculateCameraSpacePlane(reflectCamera, transform.position, transform.up));
 			reflectCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 16);
 			sharedMaterial.SetTexture(ShaderPropertyReflectTex, reflectCamera.targetTexture);
+		}
+
+		private Vector4 CalculateCameraSpacePlane(Camera cam, Vector3 pos, Vector3 normal, float sideSign = 1f)
+		{
+			Vector3 offsetPos = pos + normal * 0.07f;
+			Matrix4x4 m = cam.worldToCameraMatrix;
+			Vector3 cpos = m.MultiplyPoint(offsetPos);
+			Vector3 cnormal = m.MultiplyVector(normal).normalized * sideSign;
+			return new Vector4(cnormal.x, cnormal.y, cnormal.z, -Vector3.Dot(cpos, cnormal));
 		}
 
 		private void OnWillRenderObject()
